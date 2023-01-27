9e72f0cbd61cec46f9e9d7da8876347a02623520
nareshr183 <95609116+nareshr183@users.noreply.github.com>
2023-01-27 16:27:29 +0530

    Update main.tf

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,29 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-
- backend "azurerm" {
-    resource_group_name  = "rg-tf"
-    storage_account_name = "tfstfjan2023acc"
-    container_name       = "tfstatefile"
-    key                  = "dev.terraform.tfstate"
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-  # subscription_id = var.subscription_id
-  # client_id       = var.client_id
-  # client_secret   = var.client_secret
-  # tenant_id       = var.tenant_id
-}
-
-
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
   name     = "rg-hub"
