b03e8af1ea1a668f7d0e5086b44acd07455db2bb
nareshr183 <95609116+nareshr183@users.noreply.github.com>
2023-02-02 17:10:00 +0530

    Update main.tf

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,3 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.15.0"
+    }
+  }
+}
+
+#provider "azuread" {
+ # tenant_id = "08f4d6fc-47d1-4a61-b499-f653c720883c"
+#}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "3b8eb645-c813-405f-aa51-ab836672cf35"
+  client_id       = "62b4e4fc-ae34-4331-a2ed-fbff8d7b5574"
+  client_secret   = "_G18Q~v239KRaIQSZJElY_zjpFVH6D4iC.3_ram-"
+  tenant_id = "08f4d6fc-47d1-4a61-b499-f653c720883c"
+}
 # Create a resource group rg-hub
 resource "azurerm_resource_group" "rg" {
   name     = "rg-hub"
