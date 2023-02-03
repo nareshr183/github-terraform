5df9d0cd22d607a6f2ba683611d401b983141719
Naresh <nareshr183@gmail.com>
2023-02-03 13:04:59 +0530

    updated

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,18 +1,26 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=3.0.0"
+#     }
+#   }
 
- backend "azurerm" {
-    resource_group_name  = "rg-tf"
-    storage_account_name = "tfstfjan2023acc"
-    container_name       = "tfstatefile"
-    key                  = "dev.terraform.tfstate"
+#  backend "azurerm" {
+#     resource_group_name  = "rg-tf"
+#     storage_account_name = "tfstfjan2023acc"
+#     container_name       = "tfstatefile"
+#     key                  = "dev.terraform.tfstate"
+#   }
+# }
+
+terraform {
+  backend "azurerm" {
+
   }
 }
+
+
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
