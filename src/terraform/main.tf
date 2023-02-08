6bb1f5e399761642c86c983bf87fc04fce69e6c1
Naresh <nareshr183@gmail.com>
2023-02-08 05:46:31 +0530

    updated vm code

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -35,3 +35,108 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-hub"
   location = "East US"
 }
+
+# create virtual network
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet-Hub-01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  depends_on = [azurerm_resource_group.rg]
+}
+
+
+
+# create subnet for virtual machines
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.0.1.0/24"]
+ depends_on = [azurerm_resource_group.rg,azurerm_virtual_network.vnet1 ]
+}
+
+# create network security group and added one rule to allow all traffic 
+
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "NetworkSecurityGroup1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "nsg_rule1"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  depends_on = [azurerm_resource_group.rg ]
+}
+
+# network security group association with subnet
+
+resource "azurerm_subnet_network_security_group_association" "nsg_subnet_association" {
+
+  subnet_id                 = azurerm_subnet.subnet1.id
+  network_security_group_id = azurerm_network_security_group.nsg1.id
+  depends_on = [azurerm_subnet.subnet1,azurerm_network_security_group.nsg1]
+}
+
+# create Network interface card
+
+resource "azurerm_network_interface" "nic1" {
+  name                = "hub-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+  depends_on = [azurerm_subnet.subnet1]
+}
+
+# create ubuntu machine 1
+
+resource "azurerm_virtual_machine" "webserver-1" {
+  name                  = "webserver-1"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic1.id]
+  vm_size               = "Standard_B1ms"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+   delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+   delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "webserver-1"
+    admin_username = "naresh"
+    admin_password = "Saibaba@2834"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  depends_on = [azurerm_network_interface.nic1,azurerm_resource_group.rg]
+}
